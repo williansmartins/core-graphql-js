db0d717e1dda0edbf286a5e4b625e22d07f58865
vrto <victor.tripeno@gft.com>
2017-11-23 16:04:54 -0200

    Inclulsão de classe Verify de modo a fazer verificações simplificadas

--- src/apps/utils/verify/index.js
+++ src/apps/utils/verify/index.js
@@ -4,14 +4,19 @@ function Verify(objeto) {
 
     var erros = [];
     var elemento;
+    var prop;
 
     this.check = function(propriedade) {
         elemento = objeto[propriedade];
+        prop = propriedade;
         return this;
     }
 
     this.isEmail = function(valor) {
         valor = valor !== null && valor !== undefined ? valor : elemento;
+        /*if(!validator.isEmail(valor)) {
+            erros.push({prop : elemento});
+        }*/
         return this;
     }
 }
